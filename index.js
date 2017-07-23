3cb73a2a418806f8f6f466c00e5b0cb29699ac0f
Kyle Petrovich <kylepetrovich@gmail.com>
2017-07-23 16:32:37 -0400

    add db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
 const https = require('https');
 const Multer = require('multer');
@@ -17,6 +18,7 @@ const multer = Multer({
   }
 });
 
+let db;
 
 const app = express();
 
@@ -25,10 +27,10 @@ app.use(express.static(__dirname + '/html'));
 
 app.post('/upload', multer.single('media'), (req, res, next) => {
 
+  const collection = db.collection('timecapsules');
   const bucket = storage.bucket('timecapsules');
   // Parse out extra data to store in DB
   if (req.file) {
-
     const gcsname = `${uuidv4()}.webm`;
     const file = bucket.file(gcsname);
 
@@ -45,17 +47,30 @@ app.post('/upload', multer.single('media'), (req, res, next) => {
 
     stream.on('finish', () => {
       req.file.cloudStorageObject = gcsname;
-      res.send({ ok: true });
+
+      // If completed, insert record to DB
+      collection.insertOne({ gcsFilename: gcsname })
+        .then(() => {
+          res.send({ ok: true });
+        });
     });
 
     stream.end(req.file.buffer);
   }
 });
 
+MongoClient.connect(process.env.MONGO_URL, (err, dbHandle) => {
+  if (err !== null) {
+    console.error(err);
+    throw new Error('Failed to connect to MongoDB');
+  }
 
-https.createServer({
-  key: fs.readFileSync(process.env.SSL_KEY_PATH),
-  cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-}, app).listen(3000, () => {
-  console.log('App started on 3000');
+  https.createServer({
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+  }, app).listen(3000, () => {
+    console.log('App started on 3000');
+  });
+
+  db = dbHandle;
 });
