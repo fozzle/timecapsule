090158c6c66e5e808b0adc55eabfb00096643e9c
Kyle Petrovich <kylepetrovich@gmail.com>
2017-10-06 14:50:24 -0400

    generate signed url?

--- cloud/index.js
+++ cloud/index.js
@@ -1,11 +1,21 @@
-/**
- * Triggered from a message on a Cloud Storage bucket.
- *
- * @param {!Object} event The Cloud Functions event.
- * @param {!Function} The callback function.
- */
+const storage = require('@google-cloud/storage')();
+const bucket = storage.bucket('timecapsules');
+
 exports.createCapsule = function(event, callback) {
   console.log('Processing file: ' + event.data.name);
   console.log(event.data);
   callback();
 };
+
+exports.getSignedURL = function(req, res) {
+  const file = bucket.file(`${Date.now()}.webm`);
+  file.createResumableUpload((err, uri) => {
+    if (err) {
+      console.log('err', err);
+      res.send(err);
+    } else {
+      console.log('uri', uri);
+      res.send(uri);
+    }
+  });
+}
