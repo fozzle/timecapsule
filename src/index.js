aadcba0f87c7c2f74e6c4df8569b6f4cfb96aa29
Kyle Petrovich <kylepetrovich@gmail.com>
2017-10-07 16:37:22 -0400

    minor ui changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import adapter from 'webrtc-adapter';
 
+const API_ENDPOINT = 'https://us-central1-timecapsule-174619.cloudfunctions.net/getSignedURL';
+
 async function getMedia() {
   let stream;
   try {
@@ -11,19 +13,72 @@ async function getMedia() {
   return stream;
 }
 
-(async function() {
-  let chunks = [];
+let chunks = [];
+const targetVideo = document.querySelector('#targetVideo');
+const recordedVideo = document.querySelector('#recordedVideo');
+const recordButton = document.querySelector('#record');
+const uploadButton = document.querySelector('#upload');
+const emailField = document.querySelector('#email');
+const sendAtField = document.querySelector('#sendAt');
+const now = new Date();
+const month = now.getUTCMonth() + 1 < 10 ? `0${now.getUTCMonth() + 1}` : now.getUTCMonth() + 1;
+const date = now.getUTCDate() < 10 ? `0${now.getUTCDate()}` : now.getUTCDate();
+
+// Set min field based on today
+sendAtField.min = `${now.getUTCFullYear()}-${month}-${date}`;
+
+uploadButton.addEventListener('click', async function() {
+  const combinedBuffer = new Blob(chunks, { type: 'video/webm' });
+
+  const formData = new FormData();
+  formData.append('media', combinedBuffer);
+
+  const email = emailField.value;
+  const sendAt = sendAtField.value;
+
+  if (!email || !sendAt) {
+    return console.error('MISSING REQUIRED FIELDS');
+  }
+
+  const initiateFetchOptions = {
+    method: 'POST',
+    body: JSON.stringify({
+      email,
+      sendAt
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+
+  const putFetchOptions = {
+    method: 'PUT',
+    body: combinedBuffer,
+    headers: {
+      'Content-Length': combinedBuffer.size,
+      'Content-Type': 'video/webm',
+    },
+  };
+
+  // Get the resumable upload URL first
+  try {
+    const urlResp = await fetch(API_ENDPOINT, initiateFetchOptions);
+    const { putURL } = await urlResp.json();
+    console.log('put url', putURL);
+    const putResp = await fetch(putURL, putFetchOptions);
+  } catch (err) {
+    console.error('Error', err);
+  }
+
+  console.log('Finished putting');
+});
+
+async function init() {
   const stream = await getMedia();
-  const targetVideo = document.querySelector('#targetVideo');
-  const recordedVideo = document.querySelector('#recordedVideo');
-  const recordButton = document.querySelector('#record');
-  const uploadButton = document.querySelector('#upload');
-  const emailField = document.querySelector('#email');
-  const sendAtField = document.querySelector('#sendAt');
+
   const mediaRecorder = new MediaRecorder(stream);
 
   mediaRecorder.addEventListener('dataavailable', (e) => {
-    console.log('pushed data');
     chunks.push(e.data);
   });
 
@@ -42,53 +97,10 @@ async function getMedia() {
     }
   });
 
-  uploadButton.addEventListener('click', async function() {
-    const combinedBuffer = new Blob(chunks, { type: 'video/webm' });
-
-    const formData = new FormData();
-    formData.append('media', combinedBuffer);
-
-    const email = emailField.value;
-    const sendAt = sendAtField.value;
-
-    if (!email || !sendAt) {
-      return console.error('MISSING REQUIRED FIELDS');
-    }
-
-    const initiateFetchOptions = {
-      method: 'POST',
-      body: JSON.stringify({
-        email,
-        sendAt
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    };
-
-    const putFetchOptions = {
-      method: 'PUT',
-      body: combinedBuffer,
-      headers: {
-        'Content-Length': combinedBuffer.size,
-        'Content-Type': 'video/webm',
-      },
-    };
-
-    // Get the resumable upload URL first
-    try {
-      const urlResp = await fetch('https://us-central1-timecapsule-174619.cloudfunctions.net/getSignedURL', initiateFetchOptions);
-      const { putURL } = await urlResp.json();
-      console.log('put url', putURL);
-      const putResp = await fetch(putURL, putFetchOptions);
-    } catch (err) {
-      console.error('Error', err);
-    }
-
-    console.log('Finished putting');
-  });
-
+  recordButton.disabled = false;
 
   targetVideo.srcObject = stream;
   targetVideo.onloadedmetadata = () => targetVideo.play();
-})();
+}
+
+init();
