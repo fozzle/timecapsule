964f3f277fc5485655b79b07eb778a0270c6a98f
Kyle Petrovich <kyle@riff.tv>
2017-12-14 17:27:07 -0500

    title and icons

--- src/App.jsx
+++ src/App.jsx
@@ -19,22 +19,52 @@ export default class App extends React.Component {
 
   render() {
     const showRecorderDiv = (
-      <div
-        className="text-center"
-        style={{
-          border: '3px dashed gray',
-          color: 'gray',
-          width: '40vw',
-          padding: '16px',
-          borderRadius: '4px',
-        }}
-      >
-        <p>
-          We will need access to your camera and microphone.
-        </p>
-        <button className="btn btn-lg" onClick={() => this.setState({ recorderVisible: true })}>
-          <i className="material-icons mr-2" style={{ verticalAlign: 'bottom' }}>perm_camera_mic</i>Start
-        </button>
+      <div className="container">
+        <h3 className="text-center" style={{ margin: '16px 0' }}>Send a Video To Your Future Self</h3>
+        <div className="columns">
+          <div className="column col-md-12 text-center">
+            <div>
+              <i className="material-icons md-48">videocam</i>
+            </div>
+            <h4>Step 1</h4>
+            <p className="h5">Record your video.</p>
+          </div>
+          <div className="column col-md-12 text-center">
+            <div>
+              <i className="material-icons md-48">create</i>
+            </div>
+            <h4 className="text-center">Step 2</h4>
+            <p className="h5">Fill out your email and a date in the future.</p>
+          </div>
+          <div className="column col-md-12 text-center">
+            <div>
+              <i className="material-icons md-48">schedule</i>
+            </div>
+            <h4>Step 3</h4>
+            <p className="h5">Wait! Your video will be emailed to you on the day you specified.</p>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="col-12 column text-center">
+            <div
+              style={{
+                display: 'inline-block',
+                border: '3px dashed gray',
+                color: 'gray',
+                width: '40vw',
+                padding: '16px',
+                borderRadius: '4px',
+              }}
+            >
+              <p>
+                We will need access to your camera and microphone.
+              </p>
+              <button className="btn btn-lg" onClick={() => this.setState({ recorderVisible: true })}>
+                <i className="material-icons mr-2" style={{ verticalAlign: 'bottom' }}>perm_camera_mic</i>Start
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
     return (
@@ -69,33 +99,21 @@ export default class App extends React.Component {
             </div> : null}
         </div>
         <div className="container">
-          <h2 className="text-center" style={{ margin: '16px 0' }}>Send a Video To Your Future Self</h2>
-          <div className="columns">
-            <div className="column col-md-12">
-              <h4 className="text-center">Step 1</h4>
-              <p className="h5">After allowing us access to your camera, record the video.</p>
-            </div>
-            <div className="column col-md-12">
-              <h4 className="text-center">Step 2</h4>
-              <p className="h5">Review the recording, or don&lsquo;t. :) In any case, fill out your email and a date in the future to send it.</p>
-            </div>
-            <div className="column col-md-12">
-              <h4 className="text-center">Step 3</h4>
-              <p className="h5">Wait! Your video will be inaccessible until the date you specified, upon which you&lsquo;ll receive an email.</p>
-            </div>
-          </div>
           <h2 style={{ textAlign: 'center', margin: '16px 0' }}>FAQ</h2>
           <dl>
             <dt>Why?</dt>
             <dd>
-              <p>I just think the &ldquo;timecapsule&rdquo; concept is pretty fun!</p>
-              <p>When I was in highschool I wrote a letter during freshman year to be opened on graduation. Forgetting about it and reading it 4 years later, it was a trip to see what was important to me back then, what had changed, what hadn&lsquo;t...</p>
-              <p>Since then I&lsquo;ve journaled in the form of writing to myself. I will often forget about these musings and find them years later. They are always a joy to re-read (and terribly embarassing, but in an endearing way I think).</p>
-              <p>I thought it would be fun to move this accidental timecapsule behavior to an easy to use website. A way to quickly record a message to myself on a whim and have it removed from my grasp until some set time in the future. By putting in the cloud, the hope is that it will be resilient to failures, drive wipes, over-zealous disk cleanup, planned obscelecence, or whatever other ravages of time we subject our data to.</p>
+              <p>I just think the &ldquo;timecapsule&rdquo; concept is pretty fun! I will often forget about letters or journals I write and find them years later. They are always a joy to re-read (and terribly embarassing, but in an endearing way I think).</p>
+              <p>I thought it would be fun to move this accidental timecapsule behavior to an easy to use website. A way to quickly record a message to myself on a whim and have it removed from my grasp until some set time in the future.</p>
+              <p>By putting in the cloud, the hope is that it will be resilient to failures, drive wipes, over-zealous disk cleanup, planned obscelecence, or whatever other ravages of time we subject our data to.</p>
             </dd>
             <dt>How far in the future can I set the date?</dt>
             <dd>
               <p>I vow to keep this up for a minimum of 5 years. Whether that will be extended will be determined by the project&lsquo;s usage.</p>
+            </dd>
+            <dt>How long are the videos stored?</dt>
+            <dd>
+              <p>Until 30 days after being sent, at which point they will be erased.</p>
             </dd>
             <dt>Can I delete a previously recorded timecapsule?</dt>
             <dd>
